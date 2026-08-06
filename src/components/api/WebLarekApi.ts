b26e37ce2657112aa9704dd835ea31fa63db3f4e
Nikita Trikozin <nikitatrikozin@yandex.ru>
2026-08-07 00:58:28 +0300

    на ревью_1

--- src/components/api/WebLarekApi.ts
+++ src/components/api/WebLarekApi.ts
@@ -2,13 +2,13 @@ import { IApi } from "../../types";
 import { IProductsResponse, IOrderRequest, IOrderResponse } from "../../types";
 
 export class WebLarekApi {
-    constructor(private api: IApi) {}
+    constructor(private api: IApi) { }
 
     getProducts(): Promise<IProductsResponse> {
-    return this.api.get<IProductsResponse>('/product/');
+        return this.api.get<IProductsResponse>("/product/");
     }
 
     createOrder(data: IOrderRequest): Promise<IOrderResponse> {
-        return this.api.post<IOrderResponse>('/order/', data);
+        return this.api.post<IOrderResponse>("/order/", data);
     }
 }
--- src/components/models/ProductModel.ts
+++ src/components/models/ProductModel.ts
@@ -1,26 +1,26 @@
 import { IProduct } from "../../types";
 
 export class ProductModel {
-    private products: IProduct[] = [];
-    private preview: IProduct | null = null;
+  private products: IProduct[] = [];
+  private preview: IProduct | null = null;
 
-    setProducts(products: IProduct[]): void {
-        this.products = products;
-    }
+  setProducts(products: IProduct[]): void {
+    this.products = products;
+  }
 
-    getProducts(): IProduct[] {
-        return this.products;
-    }
+  getProducts(): IProduct[] {
+    return this.products;
+  }
 
-    getProduct(id: string): IProduct | undefined {
-        return this.products.find((product) => product.id === id);
-    }
+  getProduct(id: string): IProduct | undefined {
+    return this.products.find((product) => product.id === id);
+  }
 
-    setPreview(product: IProduct): void {
-        this.preview = product;
-    }
+  setPreview(product: IProduct): void {
+    this.preview = product;
+  }
 
-    getPreview(): IProduct | null {
-        return this.preview;
-    }
+  getPreview(): IProduct | null {
+    return this.preview;
+  }
 }
